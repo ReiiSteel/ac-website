931a97b3276af04fcfa96eab5929dcbb8b179081
Reiisteel <william.flambard@gmail.com>
2020-04-26 15:51:10 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-content>
+      <Menu/>
+    </v-content>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Menu from './components/Menu.vue';
+
+export default Vue.extend({
+  name: 'App',
+
+  components: {
+    Menu
+  },
+
+  data: () => ({
+    //
+  }),
+});
+</script>
---
+++ src/components/Menu.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-sheet id="menu-sheet">
+    <h1>Anne-Charlotte Mora</h1>
+    <h2>Golfeuse professionnelle</h2>
+    <v-tabs color="#2e7d32" centered>
+      <v-tab>Accueil</v-tab>
+      <v-tab>Birdie Fan-Club</v-tab>
+      <v-tab>Partenaires</v-tab>
+      <v-tab>Gallerie</v-tab>
+    </v-tabs>
+  </v-sheet>
+</template>
+
+<style lang="scss" scoped>
+#menu-sheet {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+}
+</style>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Menu"
+});
+</script>
