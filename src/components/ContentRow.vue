5bac1b409195696a175cdf75bc9397dec97500c2
ReiiSteel <william.flambard@gmail.com>
2020-04-30 15:17:00 +0200

    Content row Component

---
+++ src/components/ContentRow.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="d-inline-flex align-center">
+    <v-sheet class="content-row-image">
+      <v-img :src="require('@/assets/'+image)" />
+    </v-sheet>
+    <div class="content-row-text d-flex flex-column">
+      <h1 class="content-row-title title font-weight-bold">{{ title }}</h1>
+      <p class="content-row-body body-2">{{ body }}</p>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.content-row-image {
+  width: 35vw;
+}
+.content-row-text {
+  width: 30vw;
+  padding: 24px;
+
+  .content-row-title {
+    letter-spacing: 2px !important;
+    margin-bottom: 24px;
+  }
+
+  .content-row-body {
+    text-align: justify;
+  }
+}
+</style>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+const ContentRowProps = Vue.extend({
+  props: {
+    title: String,
+    body: String,
+    image: String
+  }
+});
+
+@Component
+export default class ContentRow extends ContentRowProps {}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,34 @@
 <template>
-  <div class="home">
-    <FullImageSheet />
+  <div class="home d-flex flex-column">
+    <div>
+      <FullImageSheet class="header-image" image="header_photo.jpg" />
+    </div>
+    <ContentRow
+      class="content-row-a align-self-center"
+      title="Explore the World"
+      body="Amazing tours and fun adventures are waiting for you. Treat yourself and see our offers."
+      image="content_photo_1.jpg"
+    />
   </div>
 </template>
 
+<style lang="scss" scoped>
+.content-row-a {
+  margin-top: 128px;
+  margin-bottom: 128px;
+}
+</style>
+
 <script>
 // @ is an alias to /src
-import FullImageSheet from '@/components/FullImageSheet'
+import FullImageSheet from "@/components/FullImageSheet";
+import ContentRow from "@/components/ContentRow";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    FullImageSheet
+    FullImageSheet,
+    ContentRow
   }
-}
+};
 </script>
